14852518ace361cb399037bc0955d9928bd3b474
shivani987-dev <shivanirajpoot848@gmail.com>
2024-11-02 18:26:06 +0530

    Initial commit

---
+++ src/forLoop/Table.java
@@ -0,0 +1,16 @@
+package forLoop;
+
+public class Table {
+
+	public static void main(String[] args) {
+	int n=3;
+	int mul=1;
+	while(mul<=10)
+	{
+		System.out.println(mul*n);
+		mul++;
+	}
+	
+	}
+
+}
